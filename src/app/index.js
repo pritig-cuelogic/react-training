5b0160e6d76a56f0ff1e0e402fec36f8a1101824
Priti Gupta <priti.gupta@cuelogic.co.in>
2017-02-24 18:16:30 +0530

    adding component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,14 @@
-console.log("It's working!");
+import React from "react";
+import { render } from "react-dom";
+
+class App extends React.Component {
+	render() {
+		return (
+				<div>hhh
+					<h3>Hello! </h3>
+				</div>
+			);
+	}
+}
+
+render(<App/>, window.document.getElementById("app"));
