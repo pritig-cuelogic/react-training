2e50dcc4801e548f1e690caef56be763e9e4fe91
Priti Gupta <priti.gupta@cuelogic.co.in>
2017-03-14 11:07:16 +0530

    adding redux basic

--- src/app/index.js
+++ src/app/index.js
@@ -58,7 +58,11 @@ const initialState = {
 const reducer = (state = initialState, action) => {
 	switch(action.type) {
 		case "ADD":
-		   state = state + action.payload;
+		   state = {
+		   	...state,
+		   	result: state.result + action.payload,
+		   	lastValues: [...state.lastValues, action.payload]
+		   }
 		   break;
 		case "SUBTRACT":
 		   state = state - action.payload;
